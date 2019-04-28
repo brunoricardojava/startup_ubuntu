f146bdf8ec445cf73737b3f4ab08026cfebc5c20
Bruno <brunoricardojava@gmail.com>
2019-04-28 16:38:16 -0300

    Adicionando o git e vim no script

--- pos_instalacao.sh
+++ pos_instalacao.sh
@@ -11,6 +11,8 @@ else
 	sudo apt-get install dialog
 	cmd=(dialog --separate-output --checklist "Please Select Software you want to install:" 22 76 16)
 	options=(1 "Sublime Text 3" off    # any option can be set to default to "on"
+	         2 "Git" off
+	         3 "Vim" off
 	         )
 		choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 		clear
@@ -19,11 +21,23 @@ else
 		    case $choice in
 	        	1)
 	            		#Install Sublime Text 3*
-				echo "Installing Sublime Text"
+				echo "Instalando Sublime Text"
 				wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add --y
 				echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 				apt update
 				apt install sublime-text -y
+				;;
+
+				2)
+				echo "Instalando Sublime Text"
+				apt update
+				apt install git -y
+				;;
+
+				3)
+				echo "Instalando Vim"
+				apt update
+				apt install vim -y
 				;;
 				
 	    esac
