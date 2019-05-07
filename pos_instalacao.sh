141de2328a40fdedc48db1e95d8ae8acf709fd04
Bruno Scherer <brunoricardojava@gmail.com>
2019-05-07 14:06:19 -0300

    Mais programas...

--- pos_instalacao.sh
+++ pos_instalacao.sh
@@ -14,14 +14,16 @@ else
 	cmd=(dialog --separate-output --checklist "Please Select Software you want to install:" 22 76 16)
 	options=(1 "Git" off    # any option can be set to default to "on"
 	         2 "Vim" off
-	         3 "Atom" off
-           4 "Google Chrome" off
-           5 "Terminator" off
-           6 "Android Studio" off
-           7 "Wine" off
-           8 "Nvidia Drivers" off
-           9 "Vulkan para Nvidia" off
-           10 "Lutris" off
+           3 "Oh My Zsh"
+	         4 "Atom" off
+           5 "Google Chrome" off
+           6 "Terminator" off
+           7 "Android Studio" off
+           8 "Wine" off
+           9 "Nvidia Drivers" off
+           10 "Vulkan para Nvidia" off
+           11 "Lutris" off
+           12 "Steam" off
 	         )
 		choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 		clear
@@ -41,6 +43,12 @@ else
 				;;
 
         3)
+        apt install zsh -y
+        sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+        apt-get install fonts-powerline -y
+        ;;
+
+        4)
         echo -e "\033[1;33m Instalando Atom \033[0m"
         wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
         sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
@@ -48,19 +56,20 @@ else
         apt install atom -y
 				;;
 
-        4)
+        5)
         echo -e "\033[1;33m Instalando Google Chrome \033[0m"
 				wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 				dpkg -i google-chrome-stable_current_amd64.deb
+        rm google-chrome-stable_current_amd64.deb
 				;;
 
-        5)
+        6)
         echo -e "\033[1;33m Terminator \033[0m"
 				apt update
 				apt install terminator
         ;;
 
-        6)
+        7)
         echo -e "\033[1;33m Instalando Android Studio \033[0m"
 				wget https://dl.google.com/dl/android/studio/ide-zips/3.4.0.18/android-studio-ide-183.5452501-linux.tar.gz
         echo -e "\033[5;33m Extraindo Android Studio... \033[0m"
@@ -69,7 +78,7 @@ else
         sh /opt/android-studio/bin/studio.sh
 				;;
 
-        7)
+        8)
         echo -e "\033[1;33m Instalando Wine \033[0m"
         dpkg --add-architecture i386
         wget -nc https://dl.winehq.org/wine-builds/winehq.key
@@ -80,24 +89,31 @@ else
         apt install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
         ;;
 
-        8)
+        9)
         echo -e "\033[1;33m Instalando Drivers Nvidia \033[0m"
         add-apt-repository ppa:graphics-drivers/ppa -y
         apt update
         apt install nvidia-driver-418 -y
         ;;
 
-        9)
+        10)
         echo -e "\033[1;33m Instalando Vulkan para Nvidia \033[0m"
         apt update
         apt install libvulkan1 libvulkan1:i386 -y
         ;;
 
-        10)
+        11)
         echo -e "\033[1;33m Instalando Lutris \033[0m"
         add-apt-repository ppa:lutris-team/lutris -y
         apt update
         apt install lutris -y
+        ;;
+
+        12)
+        echo -e "\033[1;33m Instalando Steam \033[0m"
+        https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+        dpkg -i steam_latest.deb
+        rm steam_latest.deb
         ;;
 
 	    esac
